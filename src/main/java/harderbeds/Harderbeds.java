60a1f89c6dca0e7635877e0af817af4015d4e53a
cashilysh <cash.cashilysh@hotmail.de>
2026-05-22 14:15:44 +0200

    update

--- src/main/java/harderbeds/Harderbeds.java
+++ src/main/java/harderbeds/Harderbeds.java
@@ -7,7 +7,7 @@ import net.fabricmc.api.ModInitializer;
 public class Harderbeds implements ModInitializer {
 
 	public static final String MOD_ID = "harderbeds";
-	public static final boolean debug = true;
+	public static final boolean debug = false;
 
 
 
